19d4f7ebb514791e262b377b16ad03888d0653cd
leandromoreirati <leandro@leandromoreirati.com.br>
2018-04-01 15:10:05 -0300

    Instalacao e Configuracao Servidor Apache (Debian e CentOS)

---
+++ README.md
@@ -0,0 +1,4 @@
+# webserver-leandromoreirati
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,3 @@
+include_recipe 'webserver-leandromoreirati::install'
+include_recipe 'webserver-leandromoreirati::configure'
+include_recipe 'webserver-leandromoreirati::service'
